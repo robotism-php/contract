9e5cc55c151f469ceb232fde62f3181dc128822a
谢毅 <2387911242@qq.com>
2021-12-05 09:46:34 +0800

    Emoji Abstract class update

--- src/Message/Item/Emoji.php
+++ src/Message/Item/Emoji.php
@@ -7,4 +7,5 @@ namespace Robotism\Contract\Message\Item;
 abstract class Emoji implements \Robotism\Contract\Message\MessageItem
 {
     abstract public function toPlain();
+    abstract public function fromPlain(PlainText $plain);
 }
