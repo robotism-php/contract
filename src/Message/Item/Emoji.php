e944088252240ddf56e0635403c15e4c04d26b6e
谢毅 <2387911242@qq.com>
2021-12-05 10:31:50 +0800

    Emoji class update

--- src/Message/Item/Emoji.php
+++ src/Message/Item/Emoji.php
@@ -7,9 +7,9 @@ namespace Robotism\Contract\Message\Item;
 class Emoji implements \Robotism\Contract\Message\MessageItem
 {
     public function toPlain(){
-
+        return new PlainText('');
     }
     static public function fromPlain(PlainText $plain){
-
+        return new Emoji();
     }
 }
